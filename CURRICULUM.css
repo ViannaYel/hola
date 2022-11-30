6c7ca9a1c1ce51954787e2b456b1af7dbbafae25
ViannaYel <119472482+ViannaYel@users.noreply.github.com>
2022-11-29 23:59:02 -0500

    Add files via upload

---
+++ CURRICULUM.css
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>CURRICULUM</title>
+        <meta name="description" content="Una guía interactiva de primeros pasos para Brackets.">
+        
+    </head>
+    <body>
+        
+     
+    <div>
+    <ul> <img align="left" src="C:\Users\Loga-Toshiba\Downloads/FOTO.jpeg" width="180px" height="180px"><h1 style="text-align: left"> Vianna Yeleny Montesdeoca Briones </h1></ul>
+   <h3 style="text-align: left;color: navy"> Dirección: Cantón Pichincha-Manabí </h3>
+<h3 style="text-align: left;color: navy"> Celular: 0960189559 </h3>
+<h3 style="text-align: left;color: navy"> E-mail: vianna.montesdeoca.b@gmail.com </h3>
+    </div>
+        
+        
+             
+        <br/>   
+<div>
+    <ul><h2 style="text-align: left"> DATOS PERSONALES </h2> 
+   <li type="script"> <h3 style="text-align: left;color: navy"> Lugar y fecha de nacimiento: Pichincha, 11 de Junio del 2005 </h3>
+   <li type="script"> <h3 style="text-align: left;color: navy"> Nacionalidad: Ecuatoriana </h3></li>
+   <li type="script"> <h3 style="text-align: left;color: navy"> Estado Civil: Soltera </h3></li>
+   <li type="script"> <h3 style="text-align: left;color: navy"> Cédula de identidad: 1314563808 </h3> </li>  </ul>
+   
+    </div>
+       
+         <br/>
+         <div>
+    <ul><h2 style="text-align: left"> FORMACIÓN ACADÉMICA </h2> 
+   <li type="script"> <h3 style="text-align: left;color: navy"> Primaria: Unidad Educativa Particular "Vicente Rocafuerte" </h3></li>
+   <li type="script"> <h3 style="text-align: left;color: navy"> Secundaria: Unidad Educativa Particular "Vicente Rocafuerte" (Cursando) </h3> </li>   </ul>
+        </div>
+        <br/>
+        
+       <div> 
+    <ul><h2 style="text-align: left"> EXPERIENCIA LABORAL </h2> 
+    <li type="script"><h3 style="text-align: left;color: navy">Presidenta del Consejo Consultivo de Adolescentes del Cantón Pichincha</h3></li> 
+    <li type="script">   <h3 style="text-align: left;color: navy"> CEO VIANNITA'S SWEETS </h3></li>     </ul>
+        <div/>
+           <br/>
+         <div> 
+    
+    <ul><h2 style="text-align: left"> CURSOS REALIZADOS </h2> 
+    <li type="script"><h3 style="text-align: left;color: navy">Objetivos de Desarrollo Sostenible</h3> </li>   <li type="script"> <h3 style="text-align: left;color: navy"> Participación de las organizaciones de la sociedad civil </h3></li>  <li type="script">   <h3 style="text-align: left;color: navy"> Derechos de la niñez y adolescencia </h3> </li> </ul>
+       <div/>
+
+       </body>
+    
+</html>     
+                                                                                            
+                            
+    
+<!--
+
+    [[[[[[[[[[[[[[[      ]]]]]]]]]]]]]]]
+    [::::::::::::::      ::::::::::::::]
+    [::::::::::::::      ::::::::::::::]
+    [::::::[[[[[[[:      :]]]]]]]::::::]
+    [:::::[                      ]:::::]
+    [:::::[                      ]:::::]
+    [:::::[                      ]:::::]
+    [:::::[                      ]:::::]
+    [:::::[     CODE THE WEB     ]:::::]
+    [:::::[  http://brackets.io  ]:::::]
+    [:::::[                      ]:::::]
+    [:::::[                      ]:::::]
+    [:::::[                      ]:::::]
+    [:::::[                      ]:::::]
+    [::::::[[[[[[[:      :]]]]]]]::::::]
+    [::::::::::::::      ::::::::::::::]
+    [::::::::::::::      ::::::::::::::]
+    [[[[[[[[[[[[[[[      ]]]]]]]]]]]]]]]
